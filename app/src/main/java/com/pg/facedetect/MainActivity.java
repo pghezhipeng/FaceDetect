723fd3fd2746de1bbab6995cf9a6c1d1546b38a0
pghezhipeng <pghezhipeng@gmail.com>
2019-10-10 22:44:48 +0800

    add permission

--- app/src/main/java/com/pg/facedetect/MainActivity.java
+++ app/src/main/java/com/pg/facedetect/MainActivity.java
@@ -1,15 +1,20 @@
 package com.pg.facedetect;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.renderscript.Allocation;
 import android.renderscript.Element;
 import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicYuvToRGB;
 import android.renderscript.Type;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.pg.facedetect.face.Box;
 import com.pg.facedetect.face.MTCNN;
@@ -34,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
     private Type.Builder yuvType, rgbaType;
     private Allocation in, out;
 
+    private final static int CAMERA_OK = 101;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,10 +58,38 @@ public class MainActivity extends AppCompatActivity {
                 .build();
     }
 
+    private boolean checkCameraPermission(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(ActivityCompat.checkSelfPermission(this,Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{android.Manifest.permission.CAMERA},CAMERA_OK);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case CAMERA_OK:
+                if (grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    //这里已经获取到了摄像头的权限，想干嘛干嘛了可以
+                    fotoapparat.start();
+                }else {
+                    //这里是拒绝给APP摄像头权限，给个提示什么的说明一下都可以。
+                    Toast.makeText(MainActivity.this,"请手动打开相机权限",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
-        fotoapparat.start();
+        if(checkCameraPermission()) {
+            fotoapparat.start();
+        }
     }
 
     @Override
@@ -67,8 +102,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void process(Frame frame) {
-            if (yuvType == null)
-            {
+            if (yuvType == null) {
                 yuvType = new Type.Builder(rs, Element.U8(rs)).setX(frame.getImage().length);
                 in = Allocation.createTyped(rs, yuvType.create(), Allocation.USAGE_SCRIPT);
 
